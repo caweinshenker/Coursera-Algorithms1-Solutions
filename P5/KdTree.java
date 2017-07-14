8a3a116c7c785f6f9245cc047036a072e8b49ef3
Colin Weinshenker <caweinshenker@email.wm.edu>
2017-07-13 22:26:53 -0700

    Insert works

--- P5/KdTree.java
+++ P5/KdTree.java
@@ -33,7 +33,7 @@ public class KdTree {
     private Node root;
     
     public KdTree() {
-        root = new Node(new Point2D(1, 1), new RectHV(0, 0, 1, 1));
+        root = null;
         size = 0;
         
     }
@@ -50,40 +50,32 @@ public class KdTree {
     
     private Node insert_helper(Node rt, Point2D p, double xmin, double ymin, double xmax, double ymax, int or) {
         if (rt == null) {
-            if (size == 0) rt = new Node(p, new RectHV(xmin, ymin, p.x(), ymax));
-            else if (size == 1) rt = new Node(p, new RectHV(xmin, p.y(), xmax, ymax));
-            else rt = new Node(p, new RectHV(xmin, ymin, xmax, ymax));
+            rt = new Node(p, new RectHV(xmin, ymin, xmax, ymax));
             size++;
         }
         else if (or == Node.XSPLIT) {
-            StdOut.println("X SPLIT");
             or = Node.YSPLIT;
             if (p.x() < rt.p.x()) rt.lb = insert_helper(rt.lb, p, xmin, ymin, rt.p.x(), ymax, or);
-            if (p.x() > rt.p.x()) rt.rt = insert_helper(rt.rt, p, rt.p.x(), ymin, xmax, ymax, or);   
+            else if (p.x() > rt.p.x()) rt.rt = insert_helper(rt.rt, p,  rt.p.x(), ymin, xmax, ymax, or);   
         }
         else if (or == Node.YSPLIT) {
-            StdOut.println("Y SPLIT");
              or = Node.XSPLIT;
              if (p.y() < rt.p.y()) rt.lb = insert_helper(rt.lb, p, xmin, ymin, xmax, rt.p.y(), or);
-             if (p.y() > rt.p.y()) rt.rt = insert_helper(rt.rt, p, xmin, rt.p.y(), xmax, ymax, or);
+             else if (p.y() > rt.p.y()) rt.rt = insert_helper(rt.rt, p, xmin, rt.p.y(), xmax, ymax, or);
         }
         return rt;
     }
     
     public void insert(Point2D p){
         if (p == null) throw new IllegalArgumentException();
-        root = insert_helper(root, p, 0.0, 0.0, 1.0, 1.0, Node.XSPLIT);
+        else root = insert_helper(root, p, 0.0, 0.0, 1.0, 1.0, Node.XSPLIT);
 
-    }
-    
-    private Node get(Point2D p){
-        return get(root, p, Node.XSPLIT);   
     }
     
     private Node get(Node rt, Point2D p, int orientation) {
         if (p == null) throw new IllegalArgumentException();
         if (rt == null) return null;
-        if (orientation == Node.XSPLIT) {
+        else if (orientation == Node.XSPLIT) {
             orientation = Node.YSPLIT;
             if (p.x() < rt.p.x()) return get(rt.lb, p, orientation);
             if (p.x() > rt.p.x()) return get(rt.rt, p, orientation);
@@ -98,28 +90,23 @@ public class KdTree {
     
     public boolean contains(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
-        return get(p) != null;
+        return get(root, p, Node.XSPLIT) != null;
        
     }
     
-    private void range(Node root,RectHV rect, ArrayList<Point2D> range) {
-        if (root == null) return;
-        if (rect.contains(root.p)) range.add(root.p);
-        if (rect.intersects(root.rect)) {
-            range(root.lb, rect, range);
-            range(root.rt, rect, range);
+    private void range(Node rt,RectHV rect, ArrayList<Point2D> range) {
+        if (rt == null) return;
+        if (rect.contains(rt.p)) range.add(rt.p);
+        if (rect.intersects(rt.rect)) {
+            range(rt.lb, rect, range);
+            range(rt.rt, rect, range);
         }
     }
     
-    private Iterable<Point2D> range(Node root, RectHV rect) {
+    public Iterable<Point2D> range(RectHV rect) {
         ArrayList<Point2D> range = new ArrayList<>();
         range(root, rect, range);
         return range;
-        
-    }
-    
-    public Iterable<Point2D> range(RectHV rect) {
-        return range(root, rect);
     }
     
     private void nodes(Node root, ArrayList<Node> nodes) {
@@ -137,67 +124,51 @@ public class KdTree {
         
     }
     
-    private Point2D nearest(Node root, Point2D p, double min_dist, int orientation) {
-        Point2D candidate1;
-        Point2D candidate2;
-        double dist = root.rect.distanceSquaredTo(p);
-        if (min_dist <= dist) return root.p;
-        else {
-            min_dist = dist;
-            if (orientation == Node.XSPLIT) {
-                if (p.x() < root.p.x()) {
-                    orientation = Node.YSPLIT;
-                    candidate1 = nearest(root.lb, p, min_dist, orientation);
-                    if (root.rect.distanceSquaredTo(candidate1) < min_dist) return candidate1;
-                    candidate2 = nearest(root.rt, p, min_dist, orientation);
-                     if (root.rect.distanceSquaredTo(candidate2) < min_dist) return candidate2;
-                }
-                else if (p.x() > root.p.x()) {
-                    candidate1 = nearest(root.rt, p, min_dist, orientation);
-                    if (root.rect.distanceSquaredTo(candidate1) < min_dist) return candidate1;
-                    candidate2 = nearest(root.lb, p, min_dist, orientation);
-                    if (root.rect.distanceSquaredTo(candidate2) < min_dist) return candidate2;
-                }
+    private Point2D nearest(Node rt, Point2D p, Point2D nearest) {
+        double min_dist = nearest.distanceSquaredTo(p);
+        double dist = rt.rect.distanceSquaredTo(p);
+        
+        if (min_dist <= dist) {
+            //Check which side of the splitting line to check first
+            if (rt.lb != null && rt.lb.rect.contains(p)) {
+                nearest = nearest(rt.lb, p, rt.p);
+                nearest = nearest(rt.rt, p, rt.p);
             }
             else {
-                orientation = Node.XSPLIT;
-                if (p.y() < root.p.y()) {
-                    candidate1 = nearest(root.lb, p, min_dist, orientation);
-                    if (root.rect.distanceSquaredTo(candidate1) < min_dist) return candidate1;
-                    candidate2 = nearest(root.rt, p, min_dist, orientation);
-                    if (root.rect.distanceSquaredTo(candidate2) < min_dist) return candidate2;
-                }
-                else if (p.y() > root.p.y()) {
-                    candidate1 = nearest(root.rt, p, min_dist, orientation);
-                    if (root.rect.distanceSquaredTo(candidate1) < min_dist) return candidate1;
-                    candidate2 = nearest(root.lb, p, min_dist, orientation);
-                    if (root.rect.distanceSquaredTo(candidate2) < min_dist) return candidate2;
-                }
+                nearest = nearest(rt.rt, p, rt.p);
+                nearest = nearest(rt.lb, p, rt.p);
             }
         }
-        return root.p;
+        return nearest;
     }
     
     public Point2D nearest(Point2D p) {
-        return nearest(root, p, Double.MAX_VALUE, Node.XSPLIT);
+        if (size == 0) return null;
+        return nearest(root, p, root.p);
         
     }
     
-    public void draw() {
-        int count = 0;
-        ArrayList<Node> nodes = (ArrayList<Node>) nodes();
-        for (int i = 1; i < nodes.size(); i++) {
-            Node n = nodes.get(i);
+    public void draw(Node rt, int orientation) {     
+            if (rt == null) return;
             StdDraw.setPenColor(StdDraw.BLACK);
             StdDraw.setPenRadius(0.01);
-            n.p.draw();
-            if (count % 2 == 0) StdDraw.setPenColor(StdDraw.RED);
-            else StdDraw.setPenColor(StdDraw.BLUE);
+            rt.p.draw();
             StdDraw.setPenRadius();
-            n.rect.draw();
-            StdOut.println(n.p.toString() + " " + n.rect.toString());
-            count++;
-        }
+            if (orientation == Node.XSPLIT) {
+               StdDraw.setPenColor(StdDraw.RED);
+               StdDraw.line(rt.p.x(), rt.rect.ymin(), rt.p.x(), rt.rect.ymax());
+            }
+            else {
+               StdDraw.setPenColor(StdDraw.BLUE);
+               StdDraw.line(rt.rect.xmin(), rt.p.y(), rt.rect.xmax(), rt.p.y());
+            }
+            orientation = (orientation == Node.XSPLIT) ? Node.YSPLIT: Node.XSPLIT;
+            draw(rt.lb, orientation);
+            draw(rt.rt, orientation);
+    }
+    
+    public void draw() {
+        draw(root, Node.XSPLIT);
     }
     
    
@@ -205,8 +176,6 @@ public class KdTree {
         
         
     }
-    
-    
     
     
 }
